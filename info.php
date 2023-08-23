8abb8a81ae309576672e40e90aecd6a0f1aa26a4
satzofficial <satheesh@spiegeltechnologies.com>
2023-08-23 15:22:32 +0530

    info message

--- info.php
+++ info.php
@@ -1,19 +1,5 @@
-<?php phpinfo(); 
+<?php phpinfo();
 ob_start();
 require_once 'index.php';
 ob_get_clean();
 return $CI;
-
-?>
-
-LTC USDT - yes
-
-ETH LTC - reverse pair
-
-ETH USDT - yes
-
-BTC LTC - reverse pair
-
-BTC USDT - yes
-
-BTC ETH - reverse pair
